1fb39c5132052fe4382d90b0acee94664e50a30b
Dash <dashhuang@gmail.com>
2025-03-03 19:21:06 +1300

    Initial Commit

---
+++ LIght/LIghtApp.swift
@@ -0,0 +1,17 @@
+//
+//  LIghtApp.swift
+//  LIght
+//
+//  Created by Dash Huang on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LIghtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LIghtTests/LIghtTests.swift
@@ -0,0 +1,17 @@
+//
+//  LIghtTests.swift
+//  LIghtTests
+//
+//  Created by Dash Huang on 03/03/2025.
+//
+
+import Testing
+@testable import LIght
+
+struct LIghtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
